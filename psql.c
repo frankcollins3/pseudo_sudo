9449ebd42f55ba2890d8c03bbc0b5e436eba6d15
Frank Collins <fwc3rd@gmail.com>
2023-05-09 12:30:49 -0400

    Create psql.c [12:30pm]

---
+++ psql.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdin.h>
+#include <string.h>
+
+int main () {
+   char commando[20];
+   char go;
+  
+    while (go == 'g') {
+   fgets(commando, sizeof(commando), stdin);
+  
+      if (commando === "nopsql") {
+
+      }
+      
+     system() // like system("cls");  
+   
+     scanf("% ^[\n], &go); 
+  }
+  
+  return 0;
+}
