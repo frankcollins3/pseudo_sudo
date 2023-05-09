e68914acb005ee6a64932a400da277d3f045e886
Frank Collins <fwc3rd@gmail.com>
2023-05-09 12:48:11 -0400

    testing kill port functionality [12:48pm]
    didnt want to stop doing what I was doing but very curious about C and the ability to create changes to the local machine system from changes in the C console

--- psql.c
+++ psql.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
-#include <stdin.h>
 #include <string.h>
+#include <stdlib.h>
+
 
 int main () {
-   char commando[20];
-   char go;
-  
-    while (go == 'g') {
-   fgets(commando, sizeof(commando), stdin);
-  
-      if (commando === "nopsql") {
+    char sentence[100];
+    char *findport = "sudo lsof -i 5432";
+    int status = system(findport);
 
-      }
-      
-     system() // like system("cls");  
-   
-     scanf("% ^[\n], &go); 
-  }
-  
-  return 0;
+    if (status == -1) {
+        printf("nothing happened\n");
+    } else {
+        printf("something happened!\n");
+        printf("heres what happened:\t %s \n", findport);
+    }
+
+    printf("please print sentence!\n");
+    fgets(sentence, sizeof(sentence), stdin);
+    printf("heres my sentence:\t %s \n", sentence);
+
+    return 0;
 }
+
+// int main () {
+//    char commando[20];
+//    char go;
+  
+//     while (go == 'g') {
+//    fgets(commando, sizeof(commando), stdin);
+  
+//       if (commando === "nopsql") {
+
+//       }
+      
+//      system() // like system("cls");  
+   
+//      scanf("% ^[\n], &go); 
+//   }
+  
+//   return 0;
+// }
